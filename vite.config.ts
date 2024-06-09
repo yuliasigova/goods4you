5e00bfac6477a94a98647ed5a38f78e1dd9e0440
yuliasigova <yulya12yulya@gmail.com>
2024-06-09 23:43:40 +0400

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import ViteSvgSpriteWrapper from "vite-svg-sprite-wrapper";
 import svgr from "vite-plugin-svgr";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()
+  plugins: [react(), 
+    svgr()
   ],
 })
